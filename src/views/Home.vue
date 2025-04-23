5f3762df0ff5542fff8e3549dda9711b5b094de2
Suchawadee39 <suchawadeeouf@gmail.com>
2025-04-23 17:33:36 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-white shadow-sm px-3">
+  <nav class="navbar navbar-expand-lg bg-white shadow-sm px-3 w-100 fixed-top z-3 ">
     <div class="container-fluid">
       <!-- Logo -->
       <a class="navbar-brand d-flex align-items-center" href="#">
@@ -24,74 +24,39 @@
 
       <!-- Menu -->
       <div class="collapse navbar-collapse" id="navbarMenu">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 h5">
           <!-- บริการ -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">บริการ</a>
-            <ul class="dropdown-menu">
+          <li class="nav-item dropdown " @mouseenter="showService = true" @mouseleave="showService = false">
+            <a class="nav-link dropdown-toggle px-4" href="#" data-bs-toggle="dropdown">บริการ</a>
+            <ul class="dropdown-menu" :class="{ show: showService }">
               <li><a class="dropdown-item" href="#">ศูนย์และคลินิก</a></li>
             </ul>
           </li>
 
           <!-- ข้อมูลเพื่อเข้ารับบริการ -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+          <li class="nav-item dropdown" @mouseenter="showPlan = true" @mouseleave="showPlan = false">
+            <a class="nav-link dropdown-toggle  px-4" href="#" data-bs-toggle="dropdown"
               >ข้อมูลเพื่อเข้ารับบริการ</a
             >
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu" :class="{ show: showPlan }">
               <li><a class="dropdown-item" href="#">ข้อมูล 1</a></li>
               <li><a class="dropdown-item" href="#">ข้อมูล 2</a></li>
             </ul>
           </li>
-          <!-- test -->
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Pookpik
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <!-- test2 -->
-          <client-only>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-          </client-only>
+   
 
           <!-- ข้อมูลสุขภาพ -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">ข้อมูลสุขภาพ</a>
-            <ul class="dropdown-menu">
+          <li class="nav-item dropdown" @mouseenter="showHealth = true" @mouseleave="showHealth = false">
+            <a class="nav-link dropdown-toggle  px-4" href="#" data-bs-toggle="dropdown">ข้อมูลสุขภาพ</a>
+            <ul class="dropdown-menu" :class="{ show: showHealth }">
               <li><a class="dropdown-item" href="#">บทความ</a></li>
             </ul>
           </li>
 
           <!-- เกี่ยวกับเรา -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">เกี่ยวกับเรา</a>
-            <ul class="dropdown-menu">
+          <li class="nav-item dropdown" @mouseenter="showAboutUs = true" @mouseleave="showAboutUs = false">
+            <a class="nav-link dropdown-toggle  px-4" href="#" data-bs-toggle="dropdown">เกี่ยวกับเรา</a>
+            <ul class="dropdown-menu" :class="{ show: showAboutUs }">
               <li><a class="dropdown-item" href="#">ประวัติ</a></li>
             </ul>
           </li>
@@ -113,3 +78,10 @@
     </div>
   </nav>
 </template>
+<script setup>
+import { ref } from 'vue'
+const showService = ref(false)
+const showPlan = ref(false)
+const showHealth= ref(false)
+const showAboutUs = ref(false)
+</script>
