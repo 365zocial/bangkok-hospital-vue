a912e9cc63dd20c304f45704b86644ef51cf3219
Suchawadee39 <suchawadeeouf@gmail.com>
2025-07-09 15:41:20 +0700

    Update centerAndClinic.vue

--- src/components/card/centerAndClinic.vue
+++ src/components/card/centerAndClinic.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container my-4">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
       <div v-for="data in dataList" :key="dataList.name" class="col">
         <div class="card shadow border-0 rounded-3 h-100 w-100 bg-white">
-          <div class="d-flex flex-column align-items-start justify-content-between" style=" padding: 1.5rem;">
+          <div
+            class="d-flex flex-row flex-sm-column align-items-start justify-content-between"
+            style="padding: 1.5rem"
+          >
             <div
-              class="d-flex justify-content-center align-items-center rounded-3 mb-2 p-3 bg-primary bg-opacity-10"
-              style="width: 4rem; height: 4rem"
-            >
-              <img :src="data.icon" :alt="dataList.name" style="width: 2.5rem; height: 2.5rem" />
-            </div>
+  class="d-flex justify-content-center align-items-center rounded-3 mb-2 p-3 bg-primary bg-opacity-10 icon-responsive"
+>
+  <img :src="data.icon" :alt="dataList.name" style="width: 2.5rem; height: 2.5rem" />
+</div>
+             
             <h5 class="card-title fw-bold mb-2" style="color: #002d73">{{ data.name }}</h5>
 
             <p
-              class="fw-light lh-sm text-truncate-2 your-element-class"
+              class="fw-light lh-sm text-truncate-2 your-element-class description-2lines"
               style="color: #627380; font-size: 0.8rem"
             >
               {{ data.description }}
@@ -44,16 +47,8 @@ const props = defineProps({
     0 3px 5px rgba(81, 126, 194, 0.1) !important;
 }
 
-/* .card:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
-} */
-
 .card:hover {
   transform: scale(1.02);
-  /* box-shadow:
-    0 8px 20px rgba(0, 102, 255, 0.12),
-    0 3px 5px rgba(81, 126, 194, 0.1) !important; */
 }
 
 .icon-wrapper {
@@ -70,8 +65,6 @@ const props = defineProps({
   /* You can override this if you have a custom primary color in your Bootstrap theme */
 }
 
-
-
 .your-element-class {
   margin: 0; /* ลบ margin ทั้งสี่ด้าน */
 }
@@ -80,15 +73,28 @@ const props = defineProps({
   font-size: 1.25rem;
 }
 
-/* .card-text {
-  font-size: 0.9rem;
-  line-height: 1.4;
-} */
 .text-truncate-2 {
   display: -webkit-box;
   -webkit-line-clamp: 2; /* จำนวนบรรทัดที่ต้องการแสดง */
   -webkit-box-orient: vertical;
   overflow: hidden;
   text-overflow: ellipsis;
+}
+
+@media (max-width: 575.98px) {
+  .description-2lines {
+    display: none !important;
+  }
+}
+
+.icon-responsive {
+  width: 4rem;
+  height: 4rem;
+}
+@media (max-width: 575.98px) {
+  .icon-responsive {
+    width: 2rem !important;
+    height: 2rem !important;
+  }
 }
 </style>
