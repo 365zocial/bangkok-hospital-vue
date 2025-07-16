71c6134f11c13db34d4850070fe5902bc654b3f5
Suchawadee39 <suchawadeeouf@gmail.com>
2025-07-16 15:14:08 +0700

    Update heart.vue

--- src/components/card/heart.vue
+++ src/components/card/heart.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="card-shadow rounded-3 h-100 w-100 bg-white border-0 cursor-pointer">
-    <div class="row padding-card mb-0">
-      <div class="col-4 rounded-3 h-100 p-0">
+    <div class="row gx-3 padding-card mb-0 align-items-center">
+      <div class="col-4 rounded-3 h-100 p-0 ">
         <img :src="data.img" :alt="data.name" class="w-100 h-100 rounded-3 object-fit-cover" />
       </div>
 
-      <div class="col-8 m-0 d-flex flex-row justify-content-between align-items-center">
-        <div class="m-0">
-          <h6 class="card-title fw-bold my-2 font-title" style="color: #002d73">
+      <div class=" p-0 col-8 m-0 d-flex flex-row justify-content-between align-items-center">
+        <div class="m-0 px-3">
+          <h6 class=" card-title fw-bold my-2 font-title" style="color: #002d73">
             {{ data.name }}
           </h6>
         </div>
 
         <div class="cov-arrow-right m-0" style="min-width: 33px">
           <div class=""><i class="bi bi-arrow-right" style="color: #002d73"></i></div>
-     
         </div>
       </div>
     </div>
@@ -58,7 +57,7 @@ const props = defineProps({
 }
 
 .padding-card {
-  padding:1rem 1rem 1rem 1.75rem;
+  padding:1rem 1.5rem 1rem 1.5rem;
 }
 .cov-arrow-right {
   width: 33px !important;
