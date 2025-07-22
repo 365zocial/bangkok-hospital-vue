2a0ae0a472e71f7831702046257a8d7435a7a566
Suchawadee39 <suchawadeeouf@gmail.com>
2025-07-22 10:58:31 +0700

    Update doctor.vue

--- src/components/card/doctor.vue
+++ src/components/card/doctor.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class=" border-0 " style="height: 100%;">
+  <div class=" border-0 card-shadow rounded-3 " style="height: 100%;">
 
-    <div class="d-flex flex-column align-items-center text-center p-4 rounded-top-3 bg-white border "
+    <div class="d-flex flex-column align-items-center text-center p-4 rounded-top-3 bg-white "
     style="height: 300px;">
       <div class="doctor-image-wrapper mb-3">
         <img
@@ -11,26 +11,26 @@
         />
       </div>
 
-      <h5 class="card-title mb-1">{{ data.title }} {{ data.name }}</h5>
+      <h5 class="card-title mb-1 fw-semibold" style="color: #363636;">{{ data.title }} {{ data.name }}</h5>
 
-      <div v-if="data.specialtyPrimary" class="text-navy mb-2">
+      <div v-if="data.specialtyPrimary" class="text-navy fw-semibold mb-2">
         {{ data.specialtyPrimary }}
       </div>
 
       <div
         v-if="data.specialtySecondary"
-        class="badge bg-light text-navy border border-primary px-3 py-2 rounded-pill mb-3"
-      >
+        class="badge fw-semibold px-3 py-2 rounded-pill mb-3 "
+      style="background-color: #E7EDFF; color: #002d73;">
         {{ data.specialtySecondary }}
       </div>
     </div>
 
-    <div class="rounded-bottom-3 d-flex w-100 " style="background-color: #f8f9f9">
-      <div class="border btn rounded-bottom-start d-flex align-items-center flex-grow-1  justify-content-center">
+    <div class="rounded-bottom-3 d-flex w-100  " style="background-color: #f8f9f9">
+      <div class=" fw-semibold border-end d-flex align-items-center flex-grow-1  justify-content-center">
         <i class="bi bi-calendar4-week me-2"></i>
         Book
       </div>
-      <div class="border btn d-flex align-items-center flex-grow-1  justify-content-center">
+      <div class=" fw-semibold d-flex align-items-center flex-grow-1  justify-content-center">
         <i class="bi bi-info-circle me-2"></i>
         Profile
       </div>
@@ -73,9 +73,9 @@ const props = defineProps({
 }
 
 .card-title {
-  font-size: 1.25rem;
-  font-weight: 600;
-  color: #333;
+  font-size: 1rem;
+  /* font-weight: 600;
+  color: #333; */
 }
 
 .card-text {
