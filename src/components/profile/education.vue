7ad025af4895e052085d9933ff5f6abf4684182e
Suchawadee39 <suchawadeeouf@gmail.com>
2025-08-20 14:56:34 +0700

    update education card
    education history

--- src/components/profile/education.vue
+++ src/components/profile/education.vue
@@ -1,88 +1,80 @@
 <template>
   <div class="">
- 
-
-
     <div class="row mt-4">
       <div class="col-md-12">
         <div class="w-100 d-flex flex-column align-items-end">
-   
-
           <div class="new-cov">
-            <div class="folded-rectangle d-flex flex-row justify-content-center align-items-start ">
-
+            <div class="folded-rectangle d-flex flex-row justify-content-center align-items-start">
               <h4 class="education-text text-center mt-3">Education</h4>
             </div>
 
             <div class="new-triangle"></div>
-            <div class="box-77 "></div>
+            <div class="box-77"></div>
             <div class="custom-box-1 w-100">
-              <div class="content w-100 ">
-
-                <div v-for="item in educationList" :key="item.year" class="d-flex flex-column flex-md-row justify-content-between align-items-start w-100 my-3">
-                  <div class="edu-year-width ">{{ item.year }}</div>
-                  <div class="edu-degree-width ">{{ item.degree }}</div>
-                  <div class="edu-institution-width ">{{ item.institution }}</div>
-
+              <div class="content w-100">
+                <div
+                  v-for="item in educationList"
+                  :key="item.year"
+                  class="d-flex flex-column flex-md-row justify-content-between align-items-start w-100 my-3"
+                >
+                  <div class="edu-year-width">{{ item.year }}</div>
+                  <div class="edu-degree-width">{{ item.degree }}</div>
+                  <div class="edu-institution-width">{{ item.institution }}</div>
                 </div>
-
               </div>
             </div>
           </div>
-
-       
         </div>
       </div>
-
-     
     </div>
   </div>
 </template>
 
 <script setup>
 const educationList = [
-  {year : "1995", degree: 'Nephrology', institution: "Chulalongkorn University, Thailand"},
-  {year : "1993", degree: 'Internal Medicine', institution: "Chulalongkorn University, Thailand"},
-  {year : "1987", degree: 'Doctor of Medicine', institution: "Chiang Mai University, Thailand"},
+  { year: '1995', degree: 'Nephrology', institution: 'Chulalongkorn University, Thailand' },
+  { year: '1993', degree: 'Internal Medicine', institution: 'Chulalongkorn University, Thailand' },
+  { year: '1987', degree: 'Doctor of Medicine', institution: 'Chiang Mai University, Thailand' },
 ]
-
 </script>
 
 <style scoped>
-.education-text{
-
+.education-text {
   font-weight: 700;
   color: #002d73;
   font-size: 1.25rem;
-
 }
 
-.edu-year-width{
-  width: 100% ;
+.edu-year-width {
+  width: 100%;
   font-weight: 700;
   color: #002d73;
   font-size: 1.25rem;
   margin: 5px 0;
 }
-.edu-degree-width{
-width: 100% ;
+.edu-degree-width {
+  width: 100%;
   font-weight: 600;
   color: #002d73;
   font-size: 1.125rem;
   margin: 5px 0;
 }
 
-.edu-institution-width{
-  width: 100% ;
+.edu-institution-width {
+  width: 100%;
   font-size: 1rem;
   font-weight: 200;
-    margin: 5px 0;
+  margin: 5px 0;
 }
 
 .custom-box-1 {
   padding: 20px 20px 4px 20px;
   border-radius: 12px;
-  background: linear-gradient(34.83deg, rgba(0, 45, 115, .12) 17.64%, rgba(0, 45, 115, .02) 71.04%);
+  background: linear-gradient(
+    34.83deg,
+    rgba(0, 45, 115, 0.12) 17.64%,
+    rgba(0, 45, 115, 0.02) 71.04%
+  );
 }
 
 /* .custom-box-1 {
@@ -90,7 +82,6 @@ width: 100% ;
   border-radius: 24px;
   background: linear-gradient(34.83deg, rgba(0, 45, 115, .12) 17.64%, rgba(0, 45, 115, .02) 71.04%);
 } */
-
 
 .new-cov {
   position: relative;
@@ -102,7 +93,7 @@ width: 100% ;
   position: absolute;
   top: 0;
   z-index: 20;
-  --r: 12px;  /* lg 24px */
+  --r: 12px; /* lg 24px */
   width: 140px;
   height: 68px; /* lg 80px */
   aspect-ratio: 1.5;
@@ -110,7 +101,8 @@ width: 100% ;
   border-top-left-radius: var(--r);
   border-bottom-right-radius: var(--r) !important;
   mask:
-    radial-gradient(var(--r) at 100% 100%, #0000 100%, #000 calc(100% + 1px)) 0 100%/var(--r) var(--r) no-repeat,
+    radial-gradient(var(--r) at 100% 100%, #0000 100%, #000 calc(100% + 1px)) 0 100% / var(--r)
+      var(--r) no-repeat,
     linear-gradient(0, #0000 var(--r), #000 0);
 }
 
@@ -119,18 +111,18 @@ width: 100% ;
   top: 0;
   z-index: 10;
   /* left: 109px; */
-   left: 96px;
+  left: 96px;
   --r: 14px;
   height: 70px;
   width: auto;
-  aspect-ratio: 1/cos(30deg);
-  --_g: calc(tan(60deg)*var(--r)) top var(--r), #000 101%, #0000 101%;
-    -webkit-mask:
-    linear-gradient(0deg,#0000 calc(3*var(--r)/2),#000 0),
-    radial-gradient(var(--r) at 50% calc(100% - 2*var(--r)),#000 98%,#0000 101%);
-  clip-path: polygon(50% 100%,100% 0,0 0);
-background: #fff;
-rotate: 0deg;
+  aspect-ratio: 1 / cos(30deg);
+  --_g: calc(tan(60deg) * var(--r)) top var(--r), #000 101%, #0000 101%;
+  -webkit-mask:
+    linear-gradient(0deg, #0000 calc(3 * var(--r) / 2), #000 0),
+    radial-gradient(var(--r) at 50% calc(100% - 2 * var(--r)), #000 98%, #0000 101%);
+  clip-path: polygon(50% 100%, 100% 0, 0 0);
+  background: #fff;
+  rotate: 0deg;
 }
 
 .content {
@@ -140,7 +132,7 @@ rotate: 0deg;
 }
 
 .box-77 {
-mask: radial-gradient(41px at 100% 100%,#0000 100%,#000);
+  mask: radial-gradient(41px at 100% 100%, #0000 100%, #000);
   background: #fff;
   width: 40px;
   height: 40px;
@@ -151,19 +143,60 @@ mask: radial-gradient(41px at 100% 100%,#0000 100%,#000);
   z-index: 10;
 }
 @media (min-width: 768px) {
-  .edu-year-width{
-  width: 25% !important;
+  .edu-year-width {
+    width: 25% !important;
+    margin: 15px 0;
+  }
+
+  .edu-degree-width {
+    width: 37.5%;
+    margin: 15px 0;
+  }
+
+  .edu-institution-width {
+    width: 37.5%;
+    margin: 15px 0;
+  }
 }
 
-.edu-degree-width{
-width: 37.5% ;
+@media (min-width: 992px) {
+  .edu-year-width {
+    margin: 10px 0;
+  }
+
+  .edu-degree-width {
+    margin: 10px 0;
+  }
+
+  .edu-institution-width {
+    margin: 10px 0;
+  }
+
+  .folded-rectangle {
+    position: absolute;
+    top: 0;
+    z-index: 20;
+    --r: 24px; 
+    width: 140px;
+    height: 80px; 
+    aspect-ratio: 1.5;
+    background: #fff;
+    border-top-left-radius: var(--r);
+    border-bottom-right-radius: var(--r) !important;
+    mask:
+      radial-gradient(var(--r) at 100% 100%, #0000 100%, #000 calc(100% + 1px)) 0 100% / var(--r)
+        var(--r) no-repeat,
+      linear-gradient(0, #0000 var(--r), #000 0);
+  }
+
+  .custom-box-1 {
+    padding: 35px 35px 15px 35px;
+    border-radius: 24px;
+    background: linear-gradient(
+      34.83deg,
+      rgba(0, 45, 115, 0.12) 17.64%,
+      rgba(0, 45, 115, 0.02) 71.04%
+    );
+  }
 }
-
-.edu-institution-width{
-  width: 37.5% ;
-
-}
-}
-
-
 </style>
